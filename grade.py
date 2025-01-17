292f689384f1e06f33c78c34f2ce0a875029df82
Nicholas Wood <horizon.nicholas14@gmail.com>
2025-01-17 17:38:27 -0500

    Add crea

---
+++ grade.py
@@ -0,0 +1,17 @@
+from add import add
+
+if __name__ == "__main__":
+    test_cases = [
+        [0, 0, 0],
+        [1, 3, 4],
+        [-1, 2, 1]
+    ]
+
+    res = {}
+
+    for num1, num2, expected_result in test_cases:
+        result = add(num1, num2)
+
+    res['grade'] = 100
+
+    print(res)
